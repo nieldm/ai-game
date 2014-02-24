2a35615efcc6fedf02801e3a9d1ff2c68e9bb25a
Daniel Mendez <nieldm@gmail.com>
2014-02-24 15:37:57 -0430

    Fix mal nombre del script

--- js/lib/hrdcdd/lib/Movements/KinematicsSteeringOutput-ck.js
+++
@@ -1,1 +0,0 @@
-define(["Phaser"],function(e){var t;t=function(){function t(){this.linear=new e.Point(0,0);this.angular=0;this.velocity=null;this.rotation=null;this.dynamic=!1}return t}();return t});
