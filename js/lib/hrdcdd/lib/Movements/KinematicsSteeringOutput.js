1464bfb3c362b6589f45ef767069d29f59ce6210
Daniel Mendez <nieldm@gmail.com>
2014-02-24 15:58:39 -0430

    Vamos vamos vamos

--- js/lib/hrdcdd/lib/Movements/KinematicsSteeringOutput.js
+++ js/lib/hrdcdd/lib/Movements/KinematicsSteeringOutput.js
@@ -4,8 +4,8 @@ define(['Phaser'], function(Phaser) {
     function KinematicSteeringOutput() {
       this.linear = new Phaser.Point(0, 0);
       this.angular = 0;
-      this.velocity = new Phaser.Point(0, 0);
-      this.rotation = 0;
+      this.velocity = null;
+      this.rotation = null;
       this.dynamic = false;
     }
 
